a31625f303f17911553fb834bb9da32f5c371072
Andrew Lock <andrewlock.net@gmail.com>
2021-07-07 23:06:09 +0100

    Start adding integration tests for source generators

---
+++ test/StronglyTypedIds.IntegrationTests/Types/GuidId.cs
@@ -0,0 +1,22 @@
+﻿using StronglyTypedIds;
+
+namespace StronglyTypedIds.IntegrationTests.Types
+{
+    [StronglyTypedId]
+    partial struct GuidId1 { }
+
+    [StronglyTypedId]
+    public partial struct GuidId2 { }
+
+    [StronglyTypedId(generateJsonConverter: false)]
+    public partial struct NoJsonGuidId { }
+
+    [StronglyTypedId(jsonConverter: StronglyTypedIdJsonConverter.NewtonsoftJson)]
+    public partial struct NewtonsoftJsonGuidId { }
+
+    [StronglyTypedId(jsonConverter: StronglyTypedIdJsonConverter.SystemTextJson)]
+    public partial struct SystemTextJsonGuidId { }
+
+    [StronglyTypedId(jsonConverter: StronglyTypedIdJsonConverter.NewtonsoftJson | StronglyTypedIdJsonConverter.SystemTextJson)]
+    public partial struct BothJsonGuidId { }
+}
