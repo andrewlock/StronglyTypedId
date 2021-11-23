abd6b84e69fd9a51042ad77b6e8886952bdf345b
Andrew Lock <andrewlock.net@gmail.com>
2021-11-23 23:09:28 +0000

    Remove unused constants

--- src/StronglyTypedIds/Constants.cs
+++ src/StronglyTypedIds/Constants.cs
@@ -2,14 +2,6 @@ namespace StronglyTypedIds
 {
     internal static class Constants
     {
-        public const string Namespace = nameof(StronglyTypedIds);
-
-        public const string StronglyTypedIdAttribute = nameof(StronglyTypedIds.Sources.StronglyTypedIdAttribute);
-        public const string FullyQualifiedStronglyTypedIdAttribute = Namespace + "." + StronglyTypedIdAttribute;
-
-        public const string StronglyTypedIdDefaultsAttribute = nameof(StronglyTypedIds.Sources.StronglyTypedIdDefaultsAttribute);
-        public const string FullyQualifiedStronglyTypedIdDefaultsAttribute = Namespace + "." + StronglyTypedIdDefaultsAttribute;
-
         public const string Usage = nameof(Usage);
     }
 }
