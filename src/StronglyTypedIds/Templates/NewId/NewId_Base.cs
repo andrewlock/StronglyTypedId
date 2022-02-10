7ff01666eba3cfccafb2405cd2558112e4f5fcc5
Khitiara <robotbrainify@gmail.com>
2022-02-10 11:52:44 -0500

    Add support for NewId package

---
+++ src/StronglyTypedIds/Templates/NewId/NewId_Base.cs
@@ -0,0 +1,24 @@
+﻿    readonly partial struct TESTID : INTERFACES
+    {
+        public MassTransit.NewId Value { get; }
+
+        public TESTID(MassTransit.NewId value)
+        {
+            Value = value;
+        }
+
+        public static TESTID New() => new TESTID(MassTransit.NewId.Next());
+        public static readonly TESTID Empty = new TESTID(MassTransit.NewId.Empty);
+
+        public bool Equals(TESTID other) => this.Value.Equals(other.Value);
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is TESTID other && Equals(other);
+        }
+
+        public override int GetHashCode() => Value.GetHashCode();
+
+        public override string ToString() => Value.ToString();
+        public static bool operator ==(TESTID a, TESTID b) => a.Equals(b);
+        public static bool operator !=(TESTID a, TESTID b) => !(a == b);
---
+++ src/StronglyTypedIds/Templates/NewId/NewId_DapperTypeHandler.cs
@@ -0,0 +1,18 @@
+﻿
+        public class DapperTypeHandler : Dapper.SqlMapper.TypeHandler<TESTID>
+        {
+            public override void SetValue(System.Data.IDbDataParameter parameter, TESTID value)
+            {
+                parameter.Value = value.Value.ToSequentialGuid();
+            }
+
+            public override TESTID Parse(object value)
+            {
+                return value switch
+                {
+                    System.Guid guidValue => new TESTID(MassTransit.NewId.FromSequentialGuid(guidValue)),
+                    string stringValue when !string.IsNullOrEmpty(stringValue) && System.Guid.TryParse(stringValue, out var result) => new TESTID(MassTransit.NewId.FromSequentialGuid(result)),
+                    _ => throw new System.InvalidCastException($"Unable to cast object of type {value.GetType()} to TESTID"),
+                };
+            }
+        }
