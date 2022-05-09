60bb3e65a7c0f9b3ff1f4fa58f47fb943d1b73ec
Marcin Rogoz <marcin.rogoz@pb.com>
2022-05-09 14:36:48 +0200

    Added missing link folder in nuget integration tests and simplified code based on review suggestion.

--- src/StronglyTypedIds/Templates/NewId/NewId_MongoSerializer.cs
+++ src/StronglyTypedIds/Templates/NewId/NewId_MongoSerializer.cs
@@ -2,11 +2,11 @@
         {
             public override TESTID Deserialize(MongoDB.Bson.Serialization.BsonDeserializationContext context, MongoDB.Bson.Serialization.BsonDeserializationArgs args)
             {
-                return new TESTID(MassTransit.NewId.FromGuid(new System.Guid(context.Reader.ReadString())));
+                return new TESTID(new MassTransit.NewId(context.Reader.ReadString()));
             }
             
             public override void Serialize(MongoDB.Bson.Serialization.BsonSerializationContext context, MongoDB.Bson.Serialization.BsonSerializationArgs args, TESTID value)
             {
-                context.Writer.WriteString(value.Value.ToGuid().ToString());
+                context.Writer.WriteString(value.Value.ToString());
             }
         }
