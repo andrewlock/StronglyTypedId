f2d63d1b062def078496e49b538c319a28b8276d
Marcin Rogoz <marcin.rogoz@pb.com>
2022-05-15 21:38:51 +0200

    Added missing empty line to ObjectId_Base template and generated snaphot files.

--- src/StronglyTypedIds/Templates/ObjectId/ObjectId_Base.cs
+++ src/StronglyTypedIds/Templates/ObjectId/ObjectId_Base.cs
@@ -22,3 +22,4 @@
         public override string ToString() => Value.ToString();
         public static bool operator ==(TESTID a, TESTID b) => a.Equals(b);
         public static bool operator !=(TESTID a, TESTID b) => !(a == b);
+        
