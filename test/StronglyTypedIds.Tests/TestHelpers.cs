45a10d0c796d01191e80df131d12fc38ee5a262f
Jonas Goronczy <goronczy.jonas@gmail.com>
2022-08-20 00:18:14 +0200

    Removes unused namespace

--- test/StronglyTypedIds.Tests/TestHelpers.cs
+++ test/StronglyTypedIds.Tests/TestHelpers.cs
@@ -3,7 +3,6 @@ using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 
