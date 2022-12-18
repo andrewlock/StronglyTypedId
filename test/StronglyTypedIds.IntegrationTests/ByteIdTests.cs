a38ed5ee792984533a2c2218ab9c462df20a6ae1
mimme <mirzamerdovic@gmail.com>
2022-12-18 02:06:01 +0100

    Add tests

---
+++ test/StronglyTypedIds.IntegrationTests/ByteIdTests.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using StronglyTypedIds.IntegrationTests.Types;
+using Xunit;
+using NewtonsoftJsonSerializer = Newtonsoft.Json.JsonConvert;
+using SystemTextJsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace StronglyTypedIds.IntegrationTests
+{
+    public class ByteIdTests
+    {
+        [Fact]
+        public void SameValuesAreEqual()
+        {
+            byte id = 123;
+            var foo1 = new ByteId(id);
+            var foo2 = new ByteId(id);
+
+            Assert.Equal(foo1, foo2);
+        }
+
+        [Fact]
+        public void EmptyValueIsEmpty()
+        {
+            Assert.Equal(0, ByteId.Empty.Value);
+        }
+
+
+        [Fact]
+        public void DifferentValuesAreUnequal()
+        {
+            var foo1 = new ByteId(1);
+            var foo2 = new ByteId(2);
+
+            Assert.NotEqual(foo1, foo2);
+        }
+
+        [Fact]
+        public void OverloadsWorkCorrectly()
+        {
+            byte id = 12;
+            var same1 = new ByteId(id);
+            var same2 = new ByteId(id);
+            var different = new ByteId(3);
+
+            Assert.True(same1 == same2);
+            Assert.False(same1 == different);
+            Assert.False(same1 != same2);
+            Assert.True(same1 != different);
+        }
+
+        [Fact]
+        public void DifferentTypesAreUnequal()
+        {
+            var bar = GuidId2.New();
+            var foo = new ByteId(23);
+
+            //Assert.NotEqual(bar, foo); // does not compile
+            Assert.NotEqual((object)bar, (object)foo);
+        }
+
+        [Fact]
+        public void CanSerializeToInt_WithNewtonsoftJsonProvider()
+        {
+            var foo = new NewtonsoftJsonByteId(123);
+
+            var serializedFoo = NewtonsoftJsonSerializer.SerializeObject(foo);
+            var serializedInt = NewtonsoftJsonSerializer.SerializeObject(foo.Value);
+
+            Assert.Equal(serializedFoo, serializedInt);
+        }
+
+        [Fact]
+        public void CanSerializeToNullableInt_WithNewtonsoftJsonProvider()
+        {
+            var entity = new EntityWithNullableId { Id = null };
+
+            var json = NewtonsoftJsonSerializer.SerializeObject(entity);
+            var deserialize = NewtonsoftJsonSerializer.DeserializeObject<EntityWithNullableId>(json);
+
+            Assert.NotNull(deserialize);
+            Assert.Null(deserialize.Id);
+        }
+
+        [Fact]
+        public void CanSerializeToInt_WithSystemTextJsonProvider()
+        {
+            var foo = new SystemTextJsonByteId(123);
+
+            var serializedFoo = SystemTextJsonSerializer.Serialize(foo);
+            var serializedInt = SystemTextJsonSerializer.Serialize(foo.Value);
+
+            Assert.Equal(serializedFoo, serializedInt);
+        }
+
+        [Fact]
+        public void CanDeserializeFromInt_WithNewtonsoftJsonProvider()
+        {
+            byte value = 123;
+            var foo = new NewtonsoftJsonByteId(value);
+            var serializedInt = NewtonsoftJsonSerializer.SerializeObject(value);
+
+            var deserializedFoo = NewtonsoftJsonSerializer.DeserializeObject<NewtonsoftJsonByteId>(serializedInt);
+
+            Assert.Equal(foo, deserializedFoo);
+        }
+
+        [Fact]
+        public void CanDeserializeFromInt_WithSystemTextJsonProvider()
+        {
+            byte value = 123;
+            var foo = new SystemTextJsonByteId(value);
+            var serializedInt = SystemTextJsonSerializer.Serialize(value);
+
+            var deserializedFoo = SystemTextJsonSerializer.Deserialize<SystemTextJsonByteId>(serializedInt);
+
+            Assert.Equal(foo, deserializedFoo);
+        }
+
+        [Fact]
+        public void CanSerializeToInt_WithBothJsonConverters()
+        {
+            var foo = new BothJsonByteId(123);
+
+            var serializedFoo1 = NewtonsoftJsonSerializer.SerializeObject(foo);
+            var serializedInt1 = NewtonsoftJsonSerializer.SerializeObject(foo.Value);
+
+            var serializedFoo2 = SystemTextJsonSerializer.Serialize(foo);
+            var serializedInt2 = SystemTextJsonSerializer.Serialize(foo.Value);
+
+            Assert.Equal(serializedFoo1, serializedInt1);
+            Assert.Equal(serializedFoo2, serializedInt2);
+        }
+
+        [Fact]
+        public void WhenNoJsonConverter_SystemTextJsonSerializesWithValueProperty()
+        {
+            var foo = new NoJsonByteId(123);
+
+            var serialized = SystemTextJsonSerializer.Serialize(foo);
+
+            var expected = "{\"Value\":" + foo.Value + "}";
+
+            Assert.Equal(expected, serialized);
+        }
+
+        [Fact]
+        public void WhenNoJsonConverter_NewtonsoftSerializesWithoutValueProperty()
+        {
+            var foo = new NoJsonByteId(123);
+
+            var serialized = NewtonsoftJsonSerializer.SerializeObject(foo);
+
+            var expected = $"\"{foo.Value}\"";
+
+            Assert.Equal(expected, serialized);
+        }
+
+        [Fact]
+        public void WhenNoTypeConverter_SerializesWithValueProperty()
+        {
+            var foo = new NoConverterByteId(123);
+
+            var newtonsoft = SystemTextJsonSerializer.Serialize(foo);
+            var systemText = SystemTextJsonSerializer.Serialize(foo);
+
+            var expected = "{\"Value\":" + foo.Value + "}";
+
+            Assert.Equal(expected, newtonsoft);
+            Assert.Equal(expected, systemText);
+        }
+
+        [Fact]
+        public void WhenEfCoreValueConverterUsesValueConverter()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<TestDbContext>()
+                .UseSqlite(connection)
+                .Options;
+
+            var original = new TestEntity { Id = new EfCoreByteId(123) };
+            using (var context = new TestDbContext(options))
+            {
+                context.Database.EnsureCreated();
+                context.Entities.Add(original);
+                context.SaveChanges();
+            }
+            using (var context = new TestDbContext(options))
+            {
+                var all = context.Entities.ToList();
+                var retrieved = Assert.Single(all);
+                Assert.Equal(original.Id, retrieved.Id);
+            }
+        }
+
+        [Fact]
+        public async Task WhenDapperValueConverterUsesValueConverter()
+        {
+            using var connection = new SqliteConnection("DataSource=:memory:");
+            await connection.OpenAsync();
+
+            var results = await connection.QueryAsync<DapperByteId>("SELECT 123");
+
+            var value = Assert.Single(results);
+            Assert.Equal(new DapperByteId(123), value);
+        }
+
+        [Theory]
+        [InlineData((byte)123)]
+        [InlineData("123")]
+        public void TypeConverter_CanConvertToAndFrom(object value)
+        {
+            var converter = TypeDescriptor.GetConverter(typeof(NoJsonByteId));
+            var id = converter.ConvertFrom(value);
+            Assert.IsType<NoJsonByteId>(id);
+            Assert.Equal(new NoJsonByteId(123), id);
+
+            var reconverted = converter.ConvertTo(id, value.GetType());
+            Assert.Equal(value, reconverted);
+        }
+
+        [Fact]
+        public void CanCompareDefaults()
+        {
+            ComparableByteId original = default;
+            var other = ComparableByteId.Empty;
+
+            var compare1 = original.CompareTo(other);
+            var compare2 = other.CompareTo(original);
+            Assert.Equal(compare1, -compare2);
+        }
+
+        [Fact]
+        public void CanEquateDefaults()
+        {
+            EquatableByteId original = default;
+            var other = EquatableByteId.Empty;
+
+            var equals1 = (original as IEquatable<EquatableByteId>).Equals(other);
+            var equals2 = (other as IEquatable<EquatableByteId>).Equals(original);
+
+            Assert.Equal(equals1, equals2);
+        }
+
+        [Fact]
+        public void ImplementsInterfaces()
+        {
+            Assert.IsAssignableFrom<IEquatable<BothByteId>>(BothByteId.Empty);
+            Assert.IsAssignableFrom<IComparable<BothByteId>>(BothByteId.Empty);
+
+            Assert.IsAssignableFrom<IEquatable<EquatableByteId>>(EquatableByteId.Empty);
+            Assert.IsAssignableFrom<IComparable<ComparableByteId>>(ComparableByteId.Empty);
+
+#pragma warning disable 184
+            Assert.False(ByteId.Empty is IComparable<ByteId>);
+            Assert.False(ByteId.Empty is IEquatable<ByteId>);
+#pragma warning restore 184
+        }
+
+
+#if NET6_0_OR_GREATER
+        [Fact]
+        public void WhenConventionBasedEfCoreValueConverterUsesValueConverter()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<ConventionsDbContext>()
+                .UseSqlite(connection)
+                .Options;
+
+            using (var context = new ConventionsDbContext(options))
+            {
+                context.Database.EnsureCreated();
+                context.Entities.Add(
+                    new TestEntity { Id = new EfCoreByteId(123) });
+                context.SaveChanges();
+            }
+            using (var context = new ConventionsDbContext(options))
+            {
+                var all = context.Entities.ToList();
+                Assert.Single(all);
+            }
+        }
+
+        public class ConventionsDbContext : DbContext
+        {
+            public DbSet<TestEntity> Entities { get; set; }
+
+            public ConventionsDbContext(DbContextOptions options) : base(options)
+            {
+            }
+
+            protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+            {
+                configurationBuilder
+                    .Properties<EfCoreByteId>()
+                    .HaveConversion<EfCoreByteId.EfCoreValueConverter>();
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder
+                    .Entity<TestEntity>(builder =>
+                    {
+                        builder
+                            .Property(x => x.Id)
+                            .ValueGeneratedNever();
+                    });
+            }
+        }
+#endif
+
+        public class TestDbContext : DbContext
+        {
+            public DbSet<TestEntity> Entities { get; set; }
+
+            public TestDbContext(DbContextOptions options) : base(options)
+            {
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder
+                    .Entity<TestEntity>(builder =>
+                    {
+                        builder
+                            .Property(x => x.Id)
+                            .HasConversion(new EfCoreByteId.EfCoreValueConverter())
+                            .ValueGeneratedNever();
+                    });
+            }
+        }
+
+        public class TestEntity
+        {
+            public EfCoreByteId Id { get; set; }
+        }
+
+        public class EntityWithNullableId
+        {
+            public NewtonsoftJsonByteId? Id { get; set; }
+        }
+    }
+}
