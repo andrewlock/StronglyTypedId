74d9576a60795968de54cfdea4605f3853558555
kkgonsovsky <126481436+kkgonsovsky@users.noreply.github.com>
2023-06-26 12:10:56 +0300

    IComparable non generic (#17)

--- src/StronglyTypedIds/Templates/NewId/NewId_IComparable.cs
+++ src/StronglyTypedIds/Templates/NewId/NewId_IComparable.cs
@@ -1,3 +1,16 @@
 ﻿        #pragma warning disable CA1036
+#nullable enable
             public int CompareTo(TESTID other) => Value.CompareTo(other.Value);
+
+            public int CompareTo(object? obj)
+            {
+                if (ReferenceEquals(obj, null))
+                    return 1;
+
+                if (obj is not TESTID other)
+                    throw new System.ArgumentException("Object is not a TESTID");
+
+                return Value.CompareTo(other.Value);
+            }
+#nullable disable
         #pragma warning restore CA1036
