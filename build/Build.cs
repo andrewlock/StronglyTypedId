0d4482f730ea21426f4c1a32bfe3fbc8d505a3ae
Marko Lahma <marko.lahma@gmail.com>
2024-10-02 10:05:51 +0300

    Upgrade to NUKE 8.1
    * use latest GitHub actions definitions

--- build/Build.cs
+++ build/Build.cs
@@ -1,15 +1,12 @@
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.CI.GitHubActions;
-using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [ShutdownDotNetAfterServerBuild]
@@ -37,16 +34,16 @@ class Build : NukeBuild
     [Parameter] readonly AbsolutePath PackagesDirectory = RootDirectory / "packages";
 
     const string NugetOrgUrl = "https://api.nuget.org/v3/index.json";
-    bool IsTag => GitHubActions.Instance?.GitHubRef?.StartsWith("refs/tags/") ?? false;
+    bool IsTag => GitHubActions.Instance?.Ref?.StartsWith("refs/tags/") ?? false;
 
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
         {
-            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
-            TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
-            EnsureCleanDirectory(PackagesDirectory);
-            EnsureCleanDirectory(ArtifactsDirectory);
+            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(x => x.DeleteDirectory());
+            TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(x => x.DeleteDirectory());
+            PackagesDirectory.CreateOrCleanDirectory();
+            ArtifactsDirectory.CreateOrCleanDirectory();
         });
 
     Target Restore => _ => _
@@ -108,8 +105,8 @@ class Build : NukeBuild
 
             if (!string.IsNullOrEmpty(PackagesDirectory))
             {
-                DeleteDirectory(PackagesDirectory / "stronglytypedid");
-                DeleteDirectory(PackagesDirectory / "stronglytypedid.attributes");
+                (PackagesDirectory / "stronglytypedid").DeleteDirectory();
+                (PackagesDirectory / "stronglytypedid.attributes").DeleteDirectory();
             }
 
             DotNetRestore(s => s
